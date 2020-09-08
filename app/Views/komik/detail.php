d30144460af8a6b75316228882f4f217d87f9738
lgridsaffan <shaffanabrorr6s@gmail.com>
2020-09-08 21:14:22 +0700

    add feature edit komik

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -15,7 +15,7 @@
                             <h5 class="card-title"><?= $komik['judul']; ?></h5>
                             <p class="card-text"><b>Penulis : <?= $komik['penulis']; ?></b></p>
                             <p class="card-text"><small class="text-muted"><b>Penerbit : <?= $komik['penerbit']; ?></b></small></p>
-                            <a href="#" class="btn btn-warning">Edit</a>
+                            <a href="/komik/edit/<?= $komik['slug']; ?>" class="btn btn-warning">Edit</a>
                             <form action="/komik/<?= $komik['id']; ?>" class="d-inline" method="post">
                                 <?= csrf_field(); ?>
                                 <input type="hidden" name="_method" value="DELETE">
