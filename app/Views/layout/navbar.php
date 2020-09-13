62b021a4e6e477abd0d801b37226516bcfc8ac04
lgridsaffan <shaffanabrorr6s@gmail.com>
2020-09-14 00:14:22 +0700

    pagination

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -19,6 +19,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/komik">Komik</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/orang">Orang</a>
+                    </li>
                 </ul>
             </div>
         </div>
